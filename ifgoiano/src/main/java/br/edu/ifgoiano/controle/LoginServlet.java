dcaf1e8132b1929c117d850b1197edc14eaa4823
2021108102540332 <2021108102540332@lab02-02.ifgoiano.local>
2024-03-19 09:44:42 -0300

    Aplicando o projeto Contatos web

--- ifgoiano/src/main/java/br/edu/ifgoiano/controle/LoginServlet.java
+++ ifgoiano/src/main/java/br/edu/ifgoiano/controle/LoginServlet.java
@@ -1,7 +1,6 @@
-package br.edu.ifgoiano;
+package br.edu.ifgoiano.controle;
 
 import java.io.IOException;
-import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,18 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/agora")
-public class AgoraServlet extends HttpServlet {
-
-	private static final long serialVersionUID = -8685905556335379708L;
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
 
 	@Override
 	public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Date data = new Date();
-
-		req.setAttribute("data", data);
-
-		req.getRequestDispatcher("datahoracert.jsp").forward(req, resp);
+		System.out.println(req.getParameter("email"));
+		System.err.println(req.getParameter("senha"));
 	}
 }
